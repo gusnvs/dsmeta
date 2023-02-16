c58941e26130175b0eb2f6e76d366acf7e4a834e
gusnvs <gagneves@hotmail.com>
2023-02-16 13:55:55 -0400

    Implements Security Config/Backend/SpringBoot

---
+++ backend/src/main/java/com/gusnvs/dsmeta/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.gusnvs.dsmeta.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.headers().frameOptions().disable();
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
+
+// todo esse código é o código de configuração para liberar o recurso Cross-origin ( que basicamente é o que permite uma aplicação acessar
+// outra que estão hospedados em lugares diferentes)
+
+// basicamente o backend está hospedado no heroku, e ele precisa acessar o frontend que está hospedado em outro lugar, então essa é a configuração de segurança
