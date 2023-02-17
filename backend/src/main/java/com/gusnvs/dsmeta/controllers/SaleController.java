f0997f5cd39fac92f5472dd9e8b98c2e9a7ab93b
gusnvs <gagneves@hotmail.com>
2023-02-17 13:46:06 -0400

    Twillio SMS

--- backend/src/main/java/com/gusnvs/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/gusnvs/dsmeta/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gusnvs.dsmeta.entities.Sale;
 import com.gusnvs.dsmeta.services.SaleService;
+import com.gusnvs.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,7 +19,9 @@ public class SaleController {
 	
 	@Autowired
 	private SaleService service;
-
+	@Autowired
+	private SmsService smsService;
+	
 	// para responder via web que é o http que é o protocolo da web
 	@GetMapping
 	public Page<Sale> findSales(
@@ -31,8 +35,11 @@ public class SaleController {
 	//Quando eu chamo o endpoint na web, ele chama no backend, vai no banco de dados
 	//Trás os dados, transforma em objeto java, em objeto (List<Sale>)
 	//Depois transforma em formato JSON, que é o formato para trafegar dados via web
-
-
+	
 	// agora vamos fazer teste pelo postman, testar as requisições
 
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
 }
